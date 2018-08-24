67f2b4646b84456de576574e1ea3e14f77c9a38e
Tyler Dailey <tkdailey11@gmail.com>
2018-08-23 20:41:47 -0600

    Prepended Filenames with our names

--- src/components/Joe.vue
+++ src/components/Joe.vue
@@ -62,7 +62,7 @@ export default {
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement('a');
           link.href = url;
-          link.setAttribute('download', file.name);
+          link.setAttribute('download', 'JoePugliano-' + file.name);
           document.body.appendChild(link);
           link.click();
         })
--- src/components/Team.vue
+++ src/components/Team.vue
@@ -61,7 +61,7 @@ export default {
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement('a');
           link.href = url;
-          link.setAttribute('download', file.name);
+          link.setAttribute('download', 'Statlete-' + file.name);
           document.body.appendChild(link);
           link.click();
         })
